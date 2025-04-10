087e2ada691c313a6fd00ec26a838d5541605f1c
도훈 <modooboiroo@gmail.com>
2025-04-10 19:29:15 +0900

    feat: JwtAuthenticationFilter

---
+++ src/main/java/com/example/demogrpcclient/global/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,45 @@
+package com.example.demogrpcclient.global.security.jwt;
+
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtAuthenticationService jwtAuthenticationService;
+    private final TokenExtractor tokenExtractor;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+
+        try {
+            tokenExtractor.extractFromHeader(authHeader)
+                    .ifPresent(token -> {
+                        Authentication authentication = jwtAuthenticationService.authenticateToken(token);
+                        SecurityContextHolder.getContext().setAuthentication(authentication);
+                    });
+
+            filterChain.doFilter(request, response);
+        } catch (JwtException e) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write(e.getMessage());
+        }
+    }
+
+}
